d928a5a2cfea766059751e15607255825d83507e
Hinako Nakai <hinako.0113n@gamil.com>
2022-08-30 20:06:51 +0900

    a

--- Techtagram/ViewController.swift
+++ Techtagram/ViewController.swift
@@ -7,13 +7,81 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+    var originalImage: UIImage!
+    
+    var filter: CIFilter!
+    @IBOutlet weak var camreImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func takePhoto(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            let picker = UIImagePickerController()
+            picker.sourceType = .camera
+            picker.delegate = self
+            picker.allowsEditing = true
+            
+            present(picker, animated: true, completion: nil)
+        }else{
+            print("error")
+        }
+    }
+    @IBAction func savePhoto(_ sender: Any) {
+        UIImageWriteToSavedPhotosAlbum(camreImageView.image!, nil, nil, nil)
+    }
+    @IBAction func colorFilter(_ sender: Any) {
+        
+        let filterImage: CIImage = CIImage(image: originalImage)!
+        
+        filter = CIFilter(name: "CIColorControls")
+        filter.setValue(filterImage, forKey: kCIInputImageKey)
+        
+        filter.setValue(1.0, forKey: "inputSaturation")
+        
+        filter.setValue(0.5, forKey: "inputBrightness")
+        
+        filter.setValue(2.5, forKey: "inputContrast")
+        
+        let ctx = CIContext(options: nil)
+        let cgImage = ctx.createCGImage(filter.outputImage!, from: filter.outputImage!.extent)
+        camreImageView.image = UIImage(cgImage: cgImage!)
+    }
+    @IBAction func openAlbum(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+            let picker = UIImagePickerController()
+            picker.sourceType = .photoLibrary
+            picker.delegate = self
+            
+            picker.allowsEditing = true
+            
+            present(picker, animated: true, completion: nil)
+        }
+    }
+    @IBAction func snsPhoto(_ sender: Any) {
+        
+        let shareText = "写真加工いえい"
+        
+        let shareImage = camreImageView.image!
+        
+        let actibityItems: [Any] = [shareText,shareImage]
+        
+        let activityViewController = UIActivityViewController(activityItems: actibityItems, applicationActivities: nil)
+        
+        let excludedActivityTypes = [UIActivity.ActivityType.postToWeibo, .saveToCameraRoll, .print]
+        
+        activityViewController.excludedActivityTypes = excludedActivityTypes
+        
+        present(activityViewController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        camreImageView.image = info[.editedImage] as? UIImage
+        
+        originalImage = camreImageView.image
+        dismiss(animated: true, completion: nil)
+    }
+    
 
 }
-
